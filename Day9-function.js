9e81f21798c54454d26cff16a4b59ecd599f8caf
Kiran Negi <142886033+kirannegi0149@users.noreply.github.com>
2025-02-26 01:01:54 +0530

    Add files via upload

---
+++ Day9-function.js
@@ -0,0 +1,47 @@
+
+ // Regular Function
+ function a (){
+    console.log("a")
+ }
+
+ //Example-1
+ // without parameter........................
+ function manoj(){
+    console.log("Hello...how are you?");
+ }
+ manoj();
+ manoj();
+ manoj();
+ 
+ //Example-2
+ //passing parameters.........................
+ function num(a,b){
+    console.log(a,b);
+}
+num(90,70);
+
+//Using alert.................................
+function ShowMessage(){
+    alert("Hello World");
+}
+ShowMessage();
+
+ //
+ // 
+ //Arrow Funtion
+ const c=(b) =>{
+    console.log("c",b);
+ }
+ //Example
+ const fun=()=>{
+    console.log("Hello...how are you?");
+ };
+ fun();
+ fun();
+ fun();
+
+
+ //Function Expression
+ const d=function(){
+    console.log("d");
+ }
