175300a04198581e9ae651964886572e69e20f09
Kiran Negi <142886033+kirannegi0149@users.noreply.github.com>
2025-02-26 23:30:39 +0530

    Update Day06-ques.js
    update

--- Day06-ques.js
+++ Day06-ques.js
@@ -4,11 +4,77 @@
 // * * * *
 // * * * * *
 
-let row=5;
-for(let i=1; i<=row; i++)
-{
-    for(let j=1; i<=j; j++)
-    {
-        console.log("*");
+const n = 5;
+let str = '';
+for (let i = 1; i <= n; i++) {
+    
+    for (let j = 1; j <= i; j++) {
+        str += '* ';
     }
+    str += '\n';
 }
+console.log(str);
+
+// * 
+// * * 
+// *   * 
+// *     * 
+// * * * * * 
+const n = 5;
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        if (j === 1 || j === i || i === n) {
+            str += '* ';
+        } else {
+            str += '  ';
+        }
+    }
+      str += '\n';
+}
+
+console.log(str);  
+
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * * 
+const n = 5;
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    
+    for (let j = 1; j <= n; j++) {
+        if (j <= n - i) {
+            str += '  ';
+        } else {
+            str += '* ';
+        }
+    }
+    str += '\n';
+}    
+
+console.log(str);
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+const n = 5;
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= n - i; j++) {
+        row += ' ';
+    }
+    for (let j = 1; j <= 2 * i - 1; j++) {
+        row += '*';
+    }
+    str += row + '\n';
+}
+
+console.log(str);
